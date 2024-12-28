1ec2735fa7cb02b08318932c34573b54d0126167
Dezired <philippherrmanns04@web.de>
2024-12-28 15:18:33 +0100

    added graphic support and changed username

--- run_container.sh
+++ run_container.sh
@@ -2,18 +2,21 @@
 ##############################################################################
 ##                            Run the container                             ##
 ##############################################################################
-SRC_CONTAINER=/home/jovyan/workspace/src
+SRC_CONTAINER=/home/group1/workspace/src
 SRC_HOST="$(pwd)"/src
-ASSETS_CONTAINER=/home/jovyan/workspace/assets
+ASSETS_CONTAINER=/home/group1/workspace/assets
 ASSETS_HOST="$(pwd)"/assets
 
 docker run \
-  --name ur10e-cell-bullet \
+  --name sorting-via-pushing \
   --privileged \
   --rm \
   -it \
   --net=host \
   -v "$SRC_HOST":"$SRC_CONTAINER":rw \
   -v "$ASSETS_HOST":"$ASSETS_CONTAINER":rw \
-  -e DISPLAY="$DISPLAY" \
- rp2024/bullet
+  -v /tmp/.X11-unix:/tmp/.X11-unix -v /mnt/wslg:/mnt/wslg \
+  -e DISPLAY="$DISPLAY" -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+  -e PULSE_SERVER=$PULSE_SERVER \
+  --gpus all \
+ rp2024/sorting-via-pushing
