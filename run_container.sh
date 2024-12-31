7a3358773e753efd01d6d22483b6fb3d91f4c33c
Dezired <philippherrmanns04@web.de>
2024-12-31 10:55:05 +0100

    added data container and port for tensorflow

--- run_container.sh
+++ run_container.sh
@@ -6,6 +6,8 @@ SRC_CONTAINER=/home/group1/workspace/src
 SRC_HOST="$(pwd)"/src
 ASSETS_CONTAINER=/home/group1/workspace/assets
 ASSETS_HOST="$(pwd)"/assets
+DATA_CONTAINER=/home/group1/workspace/data
+DATA_HOST="$(pwd)"/data
 
 docker run \
   --name sorting-via-pushing \
@@ -15,8 +17,10 @@ docker run \
   --net=host \
   -v "$SRC_HOST":"$SRC_CONTAINER":rw \
   -v "$ASSETS_HOST":"$ASSETS_CONTAINER":rw \
+  -v "$DATA_HOST":"$DATA_CONTAINER":rw \
   -v /tmp/.X11-unix:/tmp/.X11-unix -v /mnt/wslg:/mnt/wslg \
   -e DISPLAY="$DISPLAY" -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
   -e PULSE_SERVER=$PULSE_SERVER \
   --gpus all \
+  -p 6006:6006 \
  rp2024/sorting-via-pushing
