bc3d58105f3d24af1c7aa1026c1151739b6efbca
Dezired <philippherrmanns04@web.de>
2025-01-11 12:37:19 +0100

    print info and changed name form done to terminated

--- src/pushToSort.py
+++ src/pushToSort.py
@@ -1,5 +1,6 @@
 from stable_baselines3 import PPO
 from sortingViaPushingEnv import sortingViaPushingEnv as svpEnv
+from pprint import pprint
 
 MODEL_PATH = 'data/models/PPO/10000.zip'
 ENV = svpEnv()
@@ -9,8 +10,9 @@ def test_model(model_path, env):
     obs, _info = env.reset()
     for _ in range(1000):
         action, _states = model.predict(obs)
-        obs, rewards, dones, truncated, info = env.step(action)
-        if dones:
+        obs, rewards, terminated, truncated, info = env.step(action)
+        pprint(info)
+        if terminated or truncated:
             obs = env.reset()
 
 if __name__ == "__main__":
