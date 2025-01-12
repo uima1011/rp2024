a2d0aec55e538cdc4133e0b09688870aac8670ae
Dezired <philippherrmanns04@web.de>
2025-01-12 10:40:38 +0100

    added env close

--- src/checkEnv.py
+++ src/checkEnv.py
@@ -1,9 +1,14 @@
 from pprint  import pprint
 from sortingViaPushingEnv import sortingViaPushingEnv as svpEnv
 from stable_baselines3.common.env_checker import check_env
+import sys
+
+
+
+test = 1 # 1 = testEnv, 2 = manual test for x episodes
+
 env = svpEnv()
 
-test = 2 # 1 = testEnv, 2 = manual test for x episodes
 
 def moveRobotKeyboard():
     manualTermination = False
@@ -31,18 +36,26 @@ def main():
             obs = env.reset()
             if episode >0: 
                 print(f"Error: Environment was resetted (presumably after misbehaviour of robot / objects). Episode: {episode}")
-            while terminated==False: #not terminated:
-                # action = env.action_space.sample() # random actions
-                # state_obj_z = handle_objects_instance.get_state_obj_z()
-                # print(f"State object z: {state_obj_z}")
-                # input("Press Enter to continue...")
-                action, manTermination = moveRobotKeyboard() # manual actions
-                if manTermination:
-                    break
-                if action is not None:
-                    obs, reward, terminated, truncated, info = env.step(action)
+            try:
+                while terminated==False: #not terminated:
+                    # action = env.action_space.sample() # random actions
+                    # state_obj_z = handle_objects_instance.get_state_obj_z()
+                    # print(f"State object z: {state_obj_z}")
+                    # input("Press Enter to continue...")
+                    action, manTermination = moveRobotKeyboard() # manual actions
+                    if manTermination:
+                        break
+                    if action is not None:
+                        obs, reward, terminated, truncated, info = env.step(action)
+            except KeyboardInterrupt:
+                print("keyboard interrupt")
+                env.close()
     else:
         check_env(env) # test 2
+        env.close()
+
+            
+        
 
 if __name__ == '__main__':
     main()
