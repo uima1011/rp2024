4255bcc4dee8287e7b5cb9642031e7f6837d0028
Dezired <philippherrmanns04@web.de>
2025-01-12 10:44:14 +0100

    fewer objects spawned, observation space with accurate bounds, changed step reward, termination and timeout, added close function

--- src/sortingViaPushingEnv.py
+++ src/sortingViaPushingEnv.py
@@ -11,12 +11,12 @@ ASSETS_PATH = "/home/group1/workspace/assets"
 MAX_STEPS = 1000
 
 # Environment
-colours = ['green', 'red']
-objectFolders = ['signs', 'cubes']
-parts = ['plus', 'cube']
+colours = ['green'] # ['green', 'red']
+objectFolders = ['cubes'] # ['signs', 'cubes']
+parts = ['cube'] # ['plus', 'cube']
 
 ROBOT_STATE_COUNT = 2 # x and y pos
-MAX_OBJECT_COUNT = 3*len(colours)*len(parts) # max 4 of each object type and colour
+MAX_OBJECT_COUNT = 2*len(colours)*len(parts) # max 4 of each object type and colour
 GOAL_COUNT = len(colours) # red and green
 OBJECT_STATE_COUNT = 3 # x, y and rotation arround z pos
 GOAL_STATE_COUNT = 3 # x, y and rotation arround z
@@ -26,29 +26,35 @@ class sortingViaPushingEnv(gym.Env):
 	
 	def __init__(self):
 		super(sortingViaPushingEnv, self).__init__()
-		self.action_space = gym.spaces.Discrete(4) # 4 directions (forward, backward, left, right)
-		state_dim = ROBOT_STATE_COUNT + OBJECT_STATE_COUNT * MAX_OBJECT_COUNT + GOAL_STATE_COUNT * GOAL_COUNT # robot + max objects + goal states
-		self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf,
-											shape=(state_dim,), dtype=np.float64)
+		
 		self.hdlEnv = HandleEnvironment(RENDER, ASSETS_PATH)
 		self.calcReward = CalcReward(self.hdlEnv)
 		self.stepCount = 0
-		
+
+		self.action_space = gym.spaces.Discrete(4) # 4 directions (forward, backward, left, right)
+		state_dim = ROBOT_STATE_COUNT + OBJECT_STATE_COUNT * MAX_OBJECT_COUNT + GOAL_STATE_COUNT * GOAL_COUNT # robot + max objects + goal states
+		lowBounds, highBounds = self.hdlEnv.getBoundaries()
+		self.observation_space = gym.spaces.Box(low=lowBounds, high=highBounds,
+											shape=(state_dim,), dtype=np.float64)
+
 	def step(self, action):
 		self.hdlEnv.performAction(action)
-		self.reward = self.calcReward.calcReward()
-		self.terminated = self.hdlEnv.checkMisbehaviour() # TODO 
-		if self.stepCount >= MAX_STEPS-1:
-			self.truncated = True
-		else:
-			self.truncated = False # TODO ist das nicht sowieso schon false?
-		
-		
+
+		self.reward = self.calcReward.calcReward2()
+
+		self.terminated = self.calcReward.taskFinished()
+
+		self.misbehaviour = self.hdlEnv.checkMisbehaviour() # Task ended unsuccessfully (object falls from table or robot away from table)
+		self.timeout = self.calcReward.taskTimeout(self.stepCount) # timeout because robot not fullfiling task
+		self.truncated = self.misbehaviour or self.timeout
 
 		observation = self.hdlEnv.getStates()
 		info = {'Step': self.stepCount, 'Reward': self.reward, 'Action': action, 'Terminated': self.terminated, 'Truncated': self.truncated, 'Observation': observation}
 		pprint(info)
 		self.stepCount += 1
+		if self.misbehaviour:
+			self.reward = -100
+		self.reward -= 0.01 # small penalty for faster learning
 		return observation, self.reward, self.terminated, self.truncated, info
 	
 	def reset(self, seed=None):
@@ -68,3 +74,6 @@ class sortingViaPushingEnv(gym.Env):
 		info = {'Step': self.stepCount, 'Reward': self.reward, 'Action': -1, 'Terminated': self.terminated, 'Truncated': self.truncated, 'Oberservation': observation}
 		print("Environment resetted")
 		return observation, info
+	
+	def close(self):
+		self.hdlEnv.close()
