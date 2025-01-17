1a72f177b955c0bbf1a565116224c44eba68f95b
Dezired <philippherrmanns04@web.de>
2025-01-18 00:22:45 +0100

    use DQN

--- src/pushToSort.py
+++ src/pushToSort.py
@@ -1,19 +1,19 @@
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 from sortingViaPushingEnv import sortingViaPushingEnv as svpEnv
 from pprint import pprint
 
-MODEL_PATH = 'data/models/PPO/10000.zip'
+MODEL_PATH = 'data/models/DQN_normalizedObs_polarityRew_3/120000.zip'
 ENV = svpEnv()
 
 def test_model(model_path, env):
-    model = PPO.load(model_path)
+    model = DQN.load(model_path)
     obs, _info = env.reset()
     for _ in range(1000):
         action, _states = model.predict(obs)
         obs, rewards, terminated, truncated, info = env.step(action)
         pprint(info)
         if terminated or truncated:
-            obs = env.reset()
+            obs, _info = env.reset()
 
 if __name__ == "__main__":
     test_model(MODEL_PATH, ENV)
