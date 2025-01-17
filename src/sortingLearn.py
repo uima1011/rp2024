32ff6651606934f4a347dddbfcb5b91044ebb8e5
Dezired <philippherrmanns04@web.de>
2025-01-18 00:23:00 +0100

    train DQN

--- src/sortingLearn.py
+++ src/sortingLearn.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.policies import ActorCriticPolicy
 import os
 from sortingViaPushingEnv import sortingViaPushingEnv as svpEnv
@@ -11,7 +11,7 @@ class CustomMLPPolicy(ActorCriticPolicy):
 											  activation_fn= nn.ReLU,)
 
 TIMESTEPS = 10000
-MODEL = "PPO"
+MODEL = "DQN_normedRewards_notSymmetric"
 
 modelsDir = f"/home/group1/workspace/data/models/{MODEL}"
 if not os.path.exists(modelsDir):
@@ -22,7 +22,7 @@ if not os.path.exists(logDir):
 
 env = svpEnv()
 
-model = PPO(CustomMLPPolicy, env, verbose=1, tensorboard_log=logDir)
+model = DQN('MlpPolicy', env, verbose=1, tensorboard_log=logDir, device='cuda')
 # model = PPO.load(f"/home/group1/workspace/data/models/{MODEL}/10000.zip", env=env, verbose=1, tensorboard_log=logDir) # use existing model
 
 iters = 0
