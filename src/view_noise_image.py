30e25b5909906451ec7ddacea4c564d429d5f951
uima1011 <uima1011@h-ka.de>
2025-02-10 09:15:13 +0100

    deletet unused

--- src/view_noise_image.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-
-random_noise = np.random.randint(0, 256, (480, 640, 3), dtype=np.uint8)
-cv2.imshow("Random Noise", random_noise)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
