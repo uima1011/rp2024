439ce20caa37201af9ec787dee4fbc875b2d22aa
Dezired <philippherrmanns04@web.de>
2025-02-10 20:35:35 +0100

    added class describtions

--- src/handleEnvironment.py
+++ src/handleEnvironment.py
@@ -1,5 +1,5 @@
 '''
-Skript contains functions to handle the environment (objects, goals and rewards)
+    Skript contains functions to handle the environment (objects, goals and rewards)
 '''
 
 from pybullet_utils.bullet_client import BulletClient
@@ -25,6 +25,12 @@ MAX_OBJECT_PER_TYPE = cfg['objects']['number']
 MAX_OBJECT_COUNT = MAX_OBJECT_PER_TYPE*len(colours)*len(parts)
 
 class HandleEnvironment():
+    '''
+        handles the environment, robot, objects and goals
+        --> controls spawning and resetting
+        --> get positions of robot, objects and goals
+        --> check missbehaviour (out of bounds etc...)
+    '''
     def __init__(self, render, assets_folder):
         self.urdfPathRobot = os.path.join(assets_folder, 'urdf', 'robot_without_gripper.urdf')
         self.urdfPathGoal = os.path.join(assets_folder, 'objects', 'goals')
@@ -200,6 +206,9 @@ class HandleEnvironment():
         return misbehaviour
 
 class HandleObjects():
+    '''
+        generate object- and goal cords for HandleEnvironment class
+    '''
     def __init__(self, assets_folder):
         self.tableCords = {
                     'x':[0.3, 0.9], # min, max
@@ -316,6 +325,10 @@ class HandleObjects():
         print("Objects and goals resetted")
 
 class CalcReward():
+    '''
+        calculates the reward for the current state of the environment
+        --> calculates distance of robot to nearest object, object to goal and robot to goal
+    '''
     def __init__(self, handleEnv):
         self.handleEnv = handleEnv
         self.distRobToGoal, self.distObjToGoal, self.distRobToObj  = None, None, None
